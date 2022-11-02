5a2450aac4661521449caf840c7460831b55b555
Wade Jones <wadedjones@gmail.com>
2022-11-01 20:10:20 -0700

    refactored functions. restructured main() in main.py

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 # creating software to track total miles on bike
 # practice using databases with Postgres
-
-from sql_functions import insert_data, show_all, show_most_recent, total_miles
+import sys
+from sql_functions import (
+        insert_data,
+        show_all,
+        show_most_recent,
+        total_miles
+        )
 
 def main():
     greeting()
-    user_input = menu()
-    if user_input == 1:
-        show_all()
-    elif user_input == 2:
-        show_most_recent()
-    elif user_input == 3:
-        total_miles()
-    elif user_input == 4:
-        user_date, user_miles = enter_stats()
-        insert_data(user_date, user_miles)
-        print("Ride Entered!")
-        show_most_recent()
+    riding = True
+    while riding:
+        menu()
+        user_input = check_user()
+        if user_input == '1':
+            show_all()
+        elif user_input == '2':
+            show_most_recent()
+        elif user_input == '3':
+            total_miles()
+        elif user_input == '4':
+            user_date, user_miles = enter_stats()
+            insert_data(user_date, user_miles)
+        elif user_input == 'q':
+            sys.exit("\nThanks for using Bike Tracker!\n")
 
 
 def greeting():
     print("Welcome to Bike Tracker")
 
 def menu():
-    print("Would you like to: ")
-    print("\t(1). Show all rides.\n\t(2). Show recent rides.\n\t(3). Show total miles.\n\t(4). Enter a new ride.")
+    print("\nWould you like to: ")
+    print("\t(1). Show all rides.\n\t(2). Show recent rides.\n\t(3). Show total miles.\n\t(4). Enter a new ride.\n\t(Q). To quit.")
+
+def check_user():
     user_input = None
-    while user_input not in [1, 2, 3, 4]:
-        user_input = int(input("\tEnter a number (1, 2, 3, 4)\n"))
+    while user_input not in ['1', '2', '3', '4', 'q']:
+        user_input = input("\n\tEnter a number (1, 2, 3, 4) or (q) to quit.\n\t").lower()
     return user_input
 
 def check_stats():
--- sql_functions.py
+++ sql_functions.py
@@ -2,76 +2,58 @@
 from config import config
 import psycopg2
 
+def set_up_connection():
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return conn, cur
+
+def close_connection(connection, cursor):
+    try:
+        connection.commit()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
 def insert_data(_date, _miles):
     sql = """INSERT INTO stats (date, miles)
              VALUES(%s, %s);"""
     conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute(sql, (_date, _miles))
-        conn.commit()
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
+    conn, cur = set_up_connection()
+    cur.execute(sql, (_date, _miles))
+    close_connection(conn, cur)
 
 def show_all():
     conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM stats ORDER BY id")
+    conn, cur = set_up_connection()
+    cur.execute("SELECT * FROM stats ORDER BY id")
+    row = cur.fetchone()
+    while row is not None:
+        print(f"Date: {row[0]} -- Miles: {row[1]}")
         row = cur.fetchone()
-
-        while row is not None:
-            print(f"Date: {row[0]} -- Miles: {row[1]}")
-            row = cur.fetchone()
-
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    close_connection(conn, cur)
 
 def show_most_recent():
     conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM stats ORDER BY id DESC LIMIT 5")
+    conn, cur = set_up_connection()
+    cur.execute("SELECT * FROM stats ORDER BY id DESC LIMIT 5")
+    row = cur.fetchone()
+
+    while row is not None:
+        print(f"Date: {row[0]} -- Miles: {row[1]}")
         row = cur.fetchone()
-
-        while row is not None:
-            print(f"Date: {row[0]} -- Miles: {row[1]}")
-            row = cur.fetchone()
-
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    close_connection(conn, cur)
 
 def total_miles():
     conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute("SELECT SUM(miles) FROM stats")
-        row = cur.fetchone()
-        print(f"Total Miles: {row[0]}")
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    conn, cur = set_up_connection()
+    cur.execute("SELECT SUM(miles) FROM stats")
+    row = cur.fetchone()
+    print(f"Total Miles: {row[0]}")
+    close_connection(conn, cur)
